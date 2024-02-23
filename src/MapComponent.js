937e4b7338739361e6b5d84e9ef1979bee5d5f16
Christopher Simaan <simaanc@gmail.com>
2024-02-22 18:30:05 -0800

    Added Scrolling and Super Basic Routing

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,7 +1,9 @@
+import React, { useEffect } from "react";
 import L from "leaflet";
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet-routing-machine";
 
 // Correct the paths to the marker images
 delete L.Icon.Default.prototype._getIconUrl;
@@ -11,23 +13,38 @@ L.Icon.Default.mergeOptions({
 	shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
+// Define a component to add routing
+const Routing = () => {
+	const map = useMap();
+
+	useEffect(() => {
+		if (!map) return;
+
+		const routingControl = L.Routing.control({
+			waypoints: [L.latLng(34.0699, -118.4438), L.latLng(34.0689, -118.4452)],
+			routeWhileDragging: true,
+		}).addTo(map);
+
+		// Debugging: Log when the control is added
+		console.log("Routing control added");
+	}, [map]);
+
+	return null;
+};
+
 const MapComponent = () => {
 	return (
 		<MapContainer
 			center={[34.0699, -118.4438]}
 			zoom={13}
-			scrollWheelZoom={false}
-			style={{ height: "400px", width: "100%" }}
+			scrollWheelZoom={true}
+			style={{ height: "100vh", width: "100%" }}
 		>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={[34.0699, -118.4438]}>
-				<Popup>
-					A pretty CSS3 popup. <br /> Easily customizable.
-				</Popup>
-			</Marker>
+			<Routing />
 		</MapContainer>
 	);
 };
