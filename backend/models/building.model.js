a0f909ae57afd3c26e8847b1f2e0236344d1b073
Christopher Simaan <simaanc@gmail.com>
2024-03-11 23:18:11 -0700

    Changed building so message not required

--- backend/models/building.model.js
+++ backend/models/building.model.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const roomSchema = new Schema({
-  name: { type: String, required: true },
-  coords: { type: [[Number]], required: true },
-  content: { type: String, required: true },
+	name: { type: String, required: true },
+	coords: { type: [[Number]], required: true },
+	content: { type: String, required: false },
 });
 
 const floorSchema = new Schema({
-  name: { type: String, required: true },
-  rooms: [roomSchema],
+	name: { type: String, required: true },
+	rooms: [roomSchema],
 });
 
 const buildingSchema = new Schema({
-  name: { type: String, required: true },
-  coords: { type: [[Number]], required: true },
-  floors: [floorSchema],
-  center: { type: [Number] },
+	name: { type: String, required: true },
+	coords: { type: [[Number]], required: true },
+	floors: [floorSchema],
+	center: { type: [Number] },
 });
 
 const Building = mongoose.model("Building", buildingSchema);
